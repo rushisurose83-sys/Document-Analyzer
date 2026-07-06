4656b21ec05907b78a9dbfb701e5be6adf7f9a86
Rushi <rushisurose83@gmail.com>
2026-07-06 20:52:57 +0530

    the AI Bridge added

--- analyzer_blueprint.py
+++ analyzer_blueprint.py
@@ -46,5 +46,3 @@ if __name__ == "__main__":
 
     print("\n -- Text Preview -- \n")
     print(my_doc.raw_text[:600])
-                
-
---
+++ main_ai.py
@@ -0,0 +1,36 @@
+import ollama 
+from analyzer_blueprint import DocumentAnalyzer
+
+print("Starting AI Doc Pipeline....")
+
+my_doc = DocumentAnalyzer("my_resume.pdf")
+my_doc.extract_text()
+
+print("Sending Data To local Ai Model!!.....")
+
+ai_prompt = f"""
+You are an expert technical recruiter. Read the following resume and give me:
+1. The candidate's top 3 strongest technical skills.
+2. A 2-sentence summary of their best project.
+Do not include any fluff or introductory text
+
+Resume text :
+{my_doc.raw_text}
+"""
+
+try :
+    response = ollama.chat(model=my_doc.llm_model,messages=[
+        {
+            'role' : 'user' ,
+            'content' : ai_prompt
+        }
+    ])
+
+    print("\n" + "="*50)
+    print("AI Analysis Complete :")
+    print("="*50)
+    print(response['content']['message'])
+
+except Exception as e :
+    print(f"\n Ai Connection Error : {e}")
+    print("Make sure You Installed Ollama and ran 'ollama run llama3' in your terminal..!! ")
